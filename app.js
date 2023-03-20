b09f5235387cf1189aa0a343c3e2ad0c1fe98efc
Naj <43.wsalvador@gmail.com>
2023-03-20 23:07:12 +0800

    commit

--- app.js
+++ app.js
@@ -20,3 +20,65 @@ window.addEventListener('resize', () => {
     bars.setAttribute('aria-expanded', false)
   }
 })
+
+// Slider
+const navDots = document.querySelector('.navigation-dots')
+const imgArr = ['images/banner.jpg', 'images/banner-2.jpg', 'images/banner-3.jpg']
+const heroSection = document.querySelector('.hero-section')
+let isAutoSlide = false
+
+heroSection.style.backgroundImage = `url(${imgArr[0]})`
+
+const createDots = () => {
+  for(let i = 0; i < imgArr.length; i++){
+    const dot = document.createElement('i')
+    dot.setAttribute('class', 'fa-solid fa-circle-dot dot')
+    dot.setAttribute('data-index', i)
+    navDots.appendChild(dot)
+    dot.addEventListener('click', () => {
+      const dots = document.querySelectorAll('.dot')
+      dots.forEach(dot => {
+        dot.classList.remove('active')
+      })
+      heroSection.animate(fadeIn, 400)
+      heroSection.style.backgroundImage = `url(${imgArr[i]})`
+      dot.classList.add('active')
+    })
+  }
+}
+
+createDots()
+
+const dots = document.querySelectorAll('.dot')
+dots[0].classList.add('active')
+const autoSlide = () => {
+  let index = 0
+  setInterval(() => {
+    dots.forEach(dot => {
+      dot.classList.remove('active')
+    })
+    if(index > imgArr.length - 1){
+      index = 0
+      heroSection.animate(fadeIn, 400)
+      heroSection.style.backgroundImage = `url(${imgArr[index]})`
+      dots[index].classList.add('active')
+      index++
+    }else{
+      heroSection.animate(fadeIn, 400)
+      heroSection.style.backgroundImage = `url(${imgArr[index]})`
+      dots[index].classList.add('active')
+      index++
+    }
+  }, 5000)
+}
+
+if(isAutoSlide){
+  autoSlide()
+}
+
+// animation
+const fadeIn = [
+  { opacity: "0" },
+  { opacity: "1" }
+]
+
