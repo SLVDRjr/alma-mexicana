be461a223f66c70e36567425b552c673f71d3fe4
Naj <43.wsalvador@gmail.com>
2023-03-20 21:11:57 +0800

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// Mobile Menu Navigation
+const bars = document.querySelector('.nav-bars')
+const mobileNav = document.querySelector('.mobile-nav')
+
+bars.addEventListener('click', () => {
+  const visibility = mobileNav.getAttribute('data-visible')
+
+  if(visibility === 'false'){
+    mobileNav.setAttribute('data-visible', true)
+    bars.setAttribute('aria-expanded', true)
+  }else{
+    mobileNav.setAttribute('data-visible', false)
+    bars.setAttribute('aria-expanded', false)
+  }
+})
+
+window.addEventListener('resize', () => {
+  if(window.innerWidth > 992){
+    mobileNav.setAttribute('data-visible', false)
+    bars.setAttribute('aria-expanded', false)
+  }
+})
